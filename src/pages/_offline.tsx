dce8c71e9b195046407d4260b0e0719195199307
luccin243 <luccinmasirika@gmail.com>
2022-11-08 05:30:04 +0300

    offline

---
+++ src/pages/_offline.tsx
@@ -0,0 +1,8 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Offline = () => {
+  return <Typography variant="h6">Veillez vous connecter à internet</Typography>;
+};
+
+export default Offline;

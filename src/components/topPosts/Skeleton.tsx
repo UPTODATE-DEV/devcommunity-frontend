439596abf5394d59599e2eaf27112dbacb4a18bf
Luccin Masirika <53916378+luccin243@users.noreply.github.com>
2022-11-15 22:00:47 +0300

    key

--- src/components/topPosts/Skeleton.tsx
+++ src/components/topPosts/Skeleton.tsx
@@ -7,8 +7,8 @@ export function TopSkeleton() {
   return (
     <>
       {[...Array(3)].map((_, index) => (
-        <React.Fragment>
-          <Stack key={index} direction="row" spacing={2} sx={{ p: 2 }}>
+        <React.Fragment key={index}>
+          <Stack direction="row" spacing={2} sx={{ p: 2 }}>
             <Skeleton variant="circular" width={40} height={40} sx={{ flexShrink: 0 }} />
             <Stack spacing={2} sx={{ width: 1 }}>
               <Skeleton variant="rectangular" width="100%" height={20} sx={{ borderRadius: 2 }} />

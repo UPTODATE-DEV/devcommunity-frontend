676af77afa99077dfa88fb76d17a65172f1d79f5
Luccin Masirika <luccinmasirika@gmail.com>
2023-02-23 08:27:50 +0200

    menus

--- src/components/sideBars/LeftSideBar.tsx
+++ src/components/sideBars/LeftSideBar.tsx
@@ -7,19 +7,14 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Paper from "@mui/material/Paper";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
-import {
-  FcAbout,
-  FcAddressBook,
-  FcBookmark,
-  FcHome,
-  FcMindMap,
-  FcNook,
-  FcPositiveDynamic,
-  FcSettings,
-  FcSms,
-} from "react-icons/fc";
+import { FcAbout, FcBookmark, FcHome, FcNook, FcPositiveDynamic, FcSettings, FcSms } from "react-icons/fc";
+
+const ImageLogo = ({ size, url, alt }: { size: number; url: string; alt: string }) => (
+  <Image src={url} alt={alt} width={size} height={size} />
+);
 
 const LeftSideBar = () => {
   const user = useStore((state) => state.session?.user);
@@ -36,13 +31,30 @@ const LeftSideBar = () => {
     { path: "/", icon: <FcHome fontSize={28} />, label: locale === "fr" ? "Accueil" : "Home" },
     { path: "/articles", icon: <FcNook fontSize={28} />, label: "Articles" },
     { path: "/posts", icon: <FcSms fontSize={28} />, label: "Posts" },
-    { path: "/tags", icon: <FcAddressBook fontSize={28} />, label: "Tags" },
-    { path: "/blockchain", icon: <FcMindMap fontSize={28} />, label: "Blockchain" },
+    { path: "/tags", icon: <ImageLogo url="/icons/tags.png" alt="Tag Icon" size={28} />, label: "Tags" },
     { path: "/top-posts", icon: <FcPositiveDynamic fontSize={28} />, label: "Top posts" },
     {
       path: "/home",
       icon: <FcAbout fontSize={28} />,
       label: locale === "fr" ? "A propos" : "About",
+    },
+  ];
+
+  const blockchain = [
+    {
+      path: "/cardano",
+      icon: <ImageLogo url="/icons/cardano.png" alt="Cardano Logo" size={28} />,
+      label: "Cardano",
+    },
+    {
+      path: "/web3",
+      icon: <ImageLogo url="/icons/web3.png" alt="Web3 Logo" size={28} />,
+      label: "Web3",
+    },
+    {
+      path: "/blockchain",
+      icon: <ImageLogo url="/icons/blockchain.png" alt="Blockchain Logo" size={28} />,
+      label: "Blockchain",
     },
   ];
 
@@ -85,6 +97,26 @@ const LeftSideBar = () => {
           </ListItemButton>
         ))}
       </List>
+
+      <React.Fragment>
+        <Divider />
+        <List>
+          {blockchain.map(({ path, icon, label }) => (
+            <ListItemButton key={path} selected={matches(path)} onClick={() => push(path)}>
+              <ListItemIcon sx={{ mr: -1, color: matches(path) ? "primary.main" : "text.primary" }}>
+                {icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={label}
+                primaryTypographyProps={{
+                  color: matches(path) ? "primary.main" : "text.primary",
+                  fontWeight: matches(path) ? 700 : 400,
+                }}
+              />
+            </ListItemButton>
+          ))}
+        </List>
+      </React.Fragment>
 
       {user && (
         <React.Fragment>
